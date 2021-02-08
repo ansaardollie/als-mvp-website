8735eea514794f33edb5347bc99d1fc944e19576
Ansaar Dollie <ansaard123@gmail.com>
2021-02-08 19:21:16 +0200

    Add APP module components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartOverlayComponent } from './cart-overlay/cart-overlay.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CartOverlayComponent,
+    FooterComponent,
   ],
-  imports: [
+  imports: [ //TODO: Add SharedModule & HomeModule
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
